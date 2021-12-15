c6b914f8226362e4b7eca735cc5e86646e69e723
lchiang <lchiang@users.noreply.github.com>
2021-12-15 21:44:17 +0800

    2021 Q15 Done

--- 2021/15.py
+++ 2021/15.py
@@ -1,54 +1,80 @@
-from os import umask
-
-
-print()
-
 ll = open('in15.txt').read().splitlines()
 b = [[int(x) for x in l] for l in ll]
+
+'''
+# Part A
 x_max = len(b[0])-1
 y_max = len(b)-1
 
-total_risk = sum(sum(bb) for bb in b)
+def get_b(x,y):
+    return b[y][x]
+'''
+
+# Part B
+x_max = len(b[0]) * 5 - 1
+y_max = len(b) * 5 - 1
+
+def get_b(x,y):
+    w = len(b[0])
+    h = len(b)
+    inc = x//w + y//h
+    n = b[y%h][x%w] + inc
+    while n > 9: n -= 9
+    return n
 
 def print_board(bb):
     for l in bb:
         print(''.join(['{:1}'.format(c) for c in l]))
     print('-')
-
-print_board(b)
+#print_board(b)
 
 r = {} #r[(x,y)] = risk
+br = {}
 r[(0,0)] = 0
-
+br[(0,0)] = 0
 
 step = 0
-while (x_max, y_max) not in r and step < 76777:
+while (x_max, y_max) not in r and step < 260000:
     step += 1
 
     connected = {}
     c = []
-    for x,y in r.keys():
-        if y < y_max and (x,y+1) not in r: c.append((x,y+1))
-        if x < x_max and (x+1,y) not in r: c.append((x+1,y))
-        if y > 0 and (x,y-1) not in r: c.append((x,y-1))
-        if x > 0 and (x-1,y) not in r: c.append((x-1,y))
+    br_rm = []
+    for x,y in br.keys():
+        bool_rm = True
+        if y < y_max and (x,y+1) not in r:
+            bool_rm = False
+            c.append((x,y+1))
+        if x < x_max and (x+1,y) not in r:
+            bool_rm = False
+            c.append((x+1,y))
+        if y > 0 and (x,y-1) not in r:
+            bool_rm = False
+            c.append((x,y-1))
+        if x > 0 and (x-1,y) not in r:
+            bool_rm = False
+            c.append((x-1,y))
+
+        if bool_rm:
+            br_rm.append((x,y))
+
+    for rrr in br_rm: # optimisation, remove non-boundary
+        del br[rrr]
 
     c = list(set(c))
     for cx,cy in c:
-        mr = total_risk
+        mr = 99999
         if (cx,cy-1) in r: mr = min(mr, r[(cx,cy-1)])
-        if (cx,cy+1) in r: mr = min(mr, r[(cx,cy+1)])
         if (cx-1,cy) in r: mr = min(mr, r[(cx-1,cy)])
+        if (cx,cy+1) in r: mr = min(mr, r[(cx,cy+1)])
         if (cx+1,cy) in r: mr = min(mr, r[(cx+1,cy)])
-        connected[(cx,cy)] = mr + b[cy][cx]
+        connected[(cx,cy)] = mr + get_b(cx,cy)
 
     closest = min(connected, key=connected.get)
     r[closest] = connected[closest]
+    br[closest] = connected[closest]
+
+    #if step % 1000 == 0:
+    #    print('{:4} {:4} {:4} {:4}'.format(step, len(r), len(br), len(c)))
 
 print('risk', r[(x_max, y_max)])
-print(r)
-
-
-
-
-print()
