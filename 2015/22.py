ac003488e31e0d72d5176ed5ce69078ec2f7d53c
lchiang <lchiang@users.noreply.github.com>
2021-12-17 17:21:30 +0800

    2015 Q24 Done, Q22b stuck

---
+++ 2015/22.py
@@ -0,0 +1,179 @@
+import copy
+
+# https://adventofcode.com/2015/day/22
+
+class Ring:
+
+
+    '''
+    Magic Missile costs 53 mana. It instantly does 4 damage.
+    Drain costs 73 mana. It instantly does 2 damage and heals you for 2 hit points.
+    Shield costs 113 mana. It starts an effect that lasts for 6 turns. While it is active, your armor is increased by 7.
+    Poison costs 173 mana. It starts an effect that lasts for 6 turns. At the start of each turn while it is active, it deals the boss 3 damage.
+    Recharge costs 229 mana. It starts an effect that lasts for 5 turns. At the start of each turn while it is active, it gives you 101 new mana.
+    '''
+
+
+    def hitboss(self, damage):
+        self.b_hp -= max(1, damage)
+
+    def effect(self):
+        if self.timer_shield > 0:
+            self.timer_shield -= 1
+
+        if self.timer_poison > 0:
+            self.hitboss(3)
+            self.timer_poison -= 1
+
+        if self.timer_recharge > 0:
+            self.p_mana += 101
+            self.timer_recharge -= 1
+
+
+    def magic_missile(self):
+        self.mana_used += 53
+        self.p_mana -= 53
+        self.hitboss(4)
+
+    def drain(self):
+        self.mana_used += 73
+        self.p_mana -= 73
+        self.hitboss(2)
+        self.p_hp += 2
+
+    def shield(self):
+        self.mana_used += 113
+        self.p_mana -= 113
+        self.timer_shield = 6
+
+    def poison(self):
+        self.mana_used += 173
+        self.p_mana -= 173
+        self.timer_poison = 6
+
+    def recharge(self):
+        self.mana_used += 229
+        self.p_mana -= 229
+        self.timer_recharge = 5
+
+
+    def p_armor(self):
+        return 7 if self.timer_shield > 0 else 0
+
+    def print_status(self):
+        print('Player hit point {} , {} armor, {} mana'.format(self.p_hp, self.p_armor(), self.p_mana))
+        print('  Boss hit point {} '.format(self.b_hp))
+        print('Shield {}, Poison {}, Rechage {}'.format(self.timer_shield, self.timer_poison, self.timer_recharge))
+        print()
+
+    def turn(self, spell):
+        self.spell_used = self.spell_used + spell + ' '
+
+        # Player: Part B
+        self.p_hp -= 1
+        if self.p_hp <= 0: return 'b' # boss win
+
+        # Player: Effect
+        self.effect()
+        if self.b_hp <= 0: return 'p' # player win
+
+        # Player: Cast Spell
+        getattr(Ring, spell)(self)
+        if self.b_hp <= 0: return 'p' # player win
+
+        # Boss: Part B
+        self.p_hp -= 1
+        if self.p_hp <= 0: return 'b' # boss win
+
+        # Boss: Effect
+        self.effect()
+        if self.b_hp <= 0: return 'p' # player win
+
+        # Boss: Cast Spell
+        self.p_hp -= max(1, self.b_damage - self.p_armor())
+        if self.p_hp <= 0: return 'b' # boss win
+
+        return None
+
+
+
+    def __init__(self):
+        '''
+        self.p_hp = 17
+        self.p_mana = 250
+        self.b_hp = 13
+        self.b_damage = 8
+        '''
+        self.p_hp = 50
+        self.p_mana = 500
+        self.b_hp = 55
+        self.b_damage = 8
+
+
+        self.timer_shield = 0
+        self.timer_poison = 0
+        self.timer_recharge = 0
+
+        self.mana_used = 0
+        self.spell_used = ''
+
+
+    def get_avail_spells(self):
+        s = []
+        m = self.p_mana
+        if self.timer_recharge > 0:
+            m = self.p_mana + 101
+
+
+        if m >= 53:
+            s.append('magic_missile')
+        if m >= 73:
+            s.append('drain')
+        if m >= 113 and self.timer_shield == 0:
+            s.append('shield')
+        if m >= 173 and self.timer_poison == 0:
+            s.append('poison')
+        if m >= 229 and self.timer_recharge == 0:
+            s.append('recharge')
+        return s
+
+
+min_m = 9999
+def fight(ring):
+    for f in ring.get_avail_spells():
+        #if ring.mana_used > 1800: break
+        r = copy.deepcopy(ring)
+
+        # r.print_status()
+
+        result = r.turn(f)
+        if result:
+            if result == 'p':
+                global min_m
+                min_m = min(min_m, r.mana_used)
+                print(result, r.mana_used, r.spell_used, r.b_hp, r.p_hp)
+        else:
+            fight(r)
+
+
+# Part B  1382 too high
+
+r = Ring()
+
+fight(r)
+
+'''
+re = ''
+r.print_status()
+re = r.turn('poison')
+print(re)
+r.print_status()
+re = r.turn('magic_missile')
+print(re)
+r.print_status()
+re = r.turn('magic_missile')
+print(re)
+r.print_status()
+'''
+
+print(min_m)
--- 2015/24.py
+++ 2015/24.py
@@ -1,96 +1,37 @@
-ll = open('in24t.txt').read().splitlines()
+ll = open('in24.txt').read().splitlines()
 ll = [int(x) for x in ll]
 
 w = sum(ll) // 3
-
-
 pp = sorted(ll, reverse=True)
-
-print('target weight:', w)
-print(pp)
-
-g1, g2, g3 = [], [], []
-
-
-def split_two(g):
-    p = [x for x in pp if x not in g]
-    print(g,p)
-
+fewest_pack = len(ll)
+fpl = []
 
 def grp(g, limit):
     if len(g) < limit:
         p = [x for x in pp if x < min(g)] if g else pp.copy()
-        #print('<', g, p, sum(g) + sum(p))
+
         for e in p:
             ng = sum(g) + e
             np = sum(p) - e
             if ng == w:
-                print('done',g+[e])
-                split_two(g+[e])
+                global fewest_pack, fpl
+                if len(g+[e]) == fewest_pack:
+                    fpl.append(g+[e])
+                elif len(g+[e]) < fewest_pack:
+                    fewest_pack = len(g+[e])
+                    fpl.clear()
+                    fpl.append(g+[e])
+                #print('done',g+[e])
             elif ng < w and ng + np >= w:
                 grp(g+[e], limit)
 
-
-for li in range(5):
-    grp([], li)
-
-
-
-
-'''
-
-def grp(g):
-    p = [x for x in pp if x < min(g)] if g else pp.copy()
-    if sum(g) + sum(p) >= w:
-        #print('<', g, p, sum(g) + sum(p))
-        for e in p:
-            ng = sum(g) + e
-            if ng == w:
-                print('done',g+[e])
-            elif ng < w:# and ng + sum(p) > w:
-                grp(g+[e])
-
-grp([])
-
-'''
-
-'''
-
-def grp(gg,ps): #[first grp], [remain packages]
-    #print('>', gg,ps)
-    g = gg.copy()
-    rl = []
-    if sum(gg) == w:
-        #print('done', gg, ps)
-        rl.append(gg)
-    else:
-        if len(ps) > 0:
-            for e in ps:
-                if sum(g) + e <= w and (not g or min(g)>e):
-                    p = ps.copy()
-                    p.remove(e)
-                    rl = rl + grp(g+[e],p)
-    return rl
-
-
-
-
-grp1 = grp([], pp)
-
-smallest_first_group = len(ll)
-print(grp1)
-print('hi')
-for g in grp1:
-    grp2 = grp([], [x for x in pp if x not in g])
-    if len(grp2) > 0:
-        print('grp2', grp2)
-        smallest_first_group = min(smallest_first_group, len(g))
-
-
-print(smallest_first_group)
-
-
-
-#for p in pp:
-#    print(p)
-'''
+grp([], len(pp)//3)
+import math
+miqe = math.prod(ll)
+fpp = []
+for fl in fpl:
+    p = math.prod(fl)
+    if p < miqe:
+        miqe = p
+        fpp = fl
+print(miqe, fpp)
