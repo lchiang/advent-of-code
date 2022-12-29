d2f8c4c13c8c07592aa0e222c6fe564e2d22ce58
lchiang <lchiang@users.noreply.github.com>
2022-12-29 16:37:42 +0800

    2022 Q24

--- 2022/22/22.py
+++ 2022/22/22.py
@@ -6,7 +6,6 @@ h = len(ll[:-2])
 m = []
 for l in ll[:-2]:
     w = max(w, len(l))
-    #print(l)
     m.append(list(l))
 
 '''
@@ -25,11 +24,22 @@ rr = ll[-1]
 #rr = '10R5L6L2L10R1L2L7R4'
 #print(rr)
 path = re.split(r'([RL])',rr)
-
-
 visited = {}
 
-def can_walk(pos,di):
+import copy
+def print_map():
+    mm = copy.deepcopy(m)
+    for k,v in visited.items():
+        u = '.'
+        if v==3: u = '^'
+        elif v==0: u = '>'
+        elif v==1: u = 'V'
+        elif v==2: u = '<'
+        mm[k[1]][k[0]] = u
+    for y in range(h):
+        print(''.join(mm[y]))
+
+def can_walk_a(pos,di):
     x,y = pos
     if di == 0: # looking to right
         if (x+1 < len(m[y])):
@@ -49,7 +59,6 @@ def can_walk(pos,di):
             last_rock = len(m[y]) - 1 - m[y][::-1].index('#')
             if ('#' not in m[y] or
                 last_tile > last_rock):
-
                 x = last_tile
 
     elif di == 1: # looking down
@@ -57,7 +66,6 @@ def can_walk(pos,di):
         for i in range(y+1,h):
             if (x < len(m[i])) and (' ' != m[i][x]):
                 all_space_below = False
-
         if (y+1 < h) and not all_space_below:
             if ('.' == m[y+1][x]):
                 y+=1
@@ -65,8 +73,6 @@ def can_walk(pos,di):
             first_tile = h+1
             first_rock = h+1
             for i in range(h):
-
-
                 if (x < len(m[i])) and ('.' == m[i][x]):
                     first_tile = min(first_tile,i)
                 if (x < len(m[i])) and ('#' == m[i][x]):
@@ -78,10 +84,102 @@ def can_walk(pos,di):
     elif di == 3: # looking up
         all_space_above = True
         for i in range(y):
-
             if (x < len(m[i])) and (' ' != m[i][x]):
                 all_space_above = False
+        if (y-1 >= 0) and not all_space_above:
+            if ('.' == m[y-1][x]):
+                y-=1
+        else: # out of bound
+            last_tile = -1
+            last_rock = -1
+            for i in range(h):
+                if (x < len(m[i])) and ('.' == m[i][x]):
+                    last_tile = max(last_tile,i)
+                if (x < len(m[i])) and ('#' == m[i][x]):
+                    last_rock = max(last_rock,i)
 
+            if last_tile > -1 and last_rock < last_tile:
+                y=last_tile
+    #print(pos, x,y,m[y][x])
+    return ((x,y),di)
+
+def can_walk_b(pos,di):
+    x,y = pos
+    x1,y1=pos
+    di1 = di
+    if di == 0: # looking to right >
+
+        if 0<=y and y<50 and x==149: #0-49
+            print('blue edge')
+            #(149,0) > to (99,149) <
+            #(149,49) > to (99,100) <
+
+        elif y<=50 and y<100 and x==99: #50-99
+            print('green edge')
+            #(99,50) > to (100,49) ^
+            #(99,99) > to (149,49) ^
+
+        elif y<=100 and y<150 and x==99: #100-149
+            print('blue edge')
+            #(99,100) > to () <
+            #(99,149) > to () <
+
+
+        elif y<=150 and x==49: #150-199
+            print('purple edge')
+            #(49,150) > to () ^
+            #(49,199) > to () ^
+
+        else:
+            x1 = x+1
+
+
+        '''
+        if (x+1 < len(m[y])):
+            if ('.' == m[y][x+1]):
+                x+=1
+        else: # out of bound
+            if ('#' not in m[y] or
+                m[y].index('.') < m[y].index('#')):
+                x = m[y].index('.')
+        '''
+
+    elif di == 2: # looking to left <
+        if (x-1 >= 0) and (' ' != m[y][x-1]):
+            if ('.' == m[y][x-1]):
+                x-=1
+        else: # out of bound
+            last_tile = len(m[y]) - 1 - m[y][::-1].index('.')
+            last_rock = len(m[y]) - 1 - m[y][::-1].index('#')
+            if ('#' not in m[y] or
+                last_tile > last_rock):
+                x = last_tile
+
+    elif di == 1: # looking down
+        all_space_below = True
+        for i in range(y+1,h):
+            if (x < len(m[i])) and (' ' != m[i][x]):
+                all_space_below = False
+        if (y+1 < h) and not all_space_below:
+            if ('.' == m[y+1][x]):
+                y+=1
+        else: # out of bound
+            first_tile = h+1
+            first_rock = h+1
+            for i in range(h):
+                if (x < len(m[i])) and ('.' == m[i][x]):
+                    first_tile = min(first_tile,i)
+                if (x < len(m[i])) and ('#' == m[i][x]):
+                    first_rock = min(first_rock,i)
+            #print('fff',first_tile, first_rock)
+            if first_tile < h and first_tile < first_rock:
+                y=first_tile
+
+    elif di == 3: # looking up
+        all_space_above = True
+        for i in range(y):
+            if (x < len(m[i])) and (' ' != m[i][x]):
+                all_space_above = False
         if (y-1 >= 0) and not all_space_above:
             if ('.' == m[y-1][x]):
                 y-=1
@@ -97,30 +195,24 @@ def can_walk(pos,di):
             if last_tile > -1 and last_rock < last_tile:
                 y=last_tile
 
-
     #print(pos, x,y,m[y][x])
-    return (x,y)
 
-import copy
-def print_map():
-    mm = copy.deepcopy(m)
-    for k,v in visited.items():
-        u = '.'
-        if v==3: u = '^'
-        elif v==0: u = '>'
-        elif v==1: u = 'V'
-        elif v==2: u = '<'
-        mm[k[1]][k[0]] = u
-    for y in range(h):
-        print(''.join(mm[y]))
+    if ('.' == m[y1][x1]): # if can move then move
+        x=x1
+        y=y1
+        di=di1
+    return ((x,y),di)
+
+
 
 for p in path:
     if p.isnumeric():
         mv = int(p)
         #print('-',mv,'steps')
         for i in range(mv):
-            new_pos = can_walk(pos,di)
+            new_pos, new_di = can_walk_a(pos,di)
             pos = new_pos
+            di = new_di
             visited[pos] = di
             #print(i, pos, di)
 
--- 2022/24/24.py
+++ 2022/24/24.py
@@ -1,5 +1,5 @@
-ll = open('./2022/24/test1.txt').read().splitlines()
-#ll = open('./2022/24/input.txt').read().splitlines()
+ll = open('./2022/24/test.txt').read().splitlines()
+ll = open('./2022/24/input.txt').read().splitlines()
 
 def add_to_dict(x,y,symbol,dict):
     if (x,y) not in dict:
@@ -9,34 +9,31 @@ def add_to_dict(x,y,symbol,dict):
 
 w = len(ll[0])
 h = len(ll)
+end_pos = (w-2,h-1)
+
 blizzard = {}
 for y in range(len(ll)):
     for x in range(len(ll[0])):
         if ll[y][x] in ['>','v','^','<']:
             add_to_dict(x,y,ll[y][x],blizzard)
+#print(blizzard)
 
-
-print(blizzard)
-
-def print_map(blizzard):
+def print_map(blizzard,pos):
     for y in range(h):
         line = []
         for x in range(w):
-            if (x,y) in blizzard.keys():
+            if (x,y) == pos:
+                line.append('E')
+            elif (x,y) in blizzard.keys():
                 v = blizzard[(x,y)]
                 line.append(str(len(v)) if len(v)>1 else v[0])
-            elif (x,y)==(1,0) or (x,y)==(w-2,h-1):
+            elif (x,y)==(1,0) or (x,y)==end_pos:
                 line.append('.')
             elif x==0 or x==w-1 or y==0 or y==h-1:
                 line.append('#')
             else:
                 line.append('.')
-
         print(''.join(line))
-    print()
-
-print_map(blizzard)
-
 
 
 def move(blizzard, step):
@@ -51,7 +48,6 @@ def move(blizzard, step):
                 x = (x0-step-1)%(w-2)+1
                 add_to_dict(x,y0,'<',b)
             elif v=='v':
-
                 y = (y0+step-1)%(h-2)+1
                 add_to_dict(x0,y,'v',b)
             elif v=='^':
@@ -59,18 +55,89 @@ def move(blizzard, step):
                 add_to_dict(x0,y,'^',b)
             else:
                 print('ERR: Unknown symbol',v)
-
-
-
-
-
     return b
 
-b1 = move(blizzard,1)
-print_map(b1)
-b1 = move(blizzard,2)
-print_map(b1)
-b1 = move(blizzard,3)
-print_map(b1)
-b1 = move(blizzard,4)
-print_map(b1)
+def can_walk(map,prev_pos):
+    x,y = prev_pos
+    re = set()
+    if (x,y+1)==end_pos:
+        re.add((end_pos))
+    else:
+        around = [(x,y),(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+        for aa in around:
+            xa, ya = aa
+            if ((aa == (1,0)) or (aa == (w-2,h-1)) or
+                (xa>0 and xa<w-1 and ya>0 and ya<h-1 and ((xa,ya) not in map))):
+                re.add((xa,ya))
+    return re
+
+
+import time
+start = time.time()
+
+#print_map(blizzard,pos)
+
+p_set = set()
+p_set.add((1,0))
+
+status = 0
+turn_around = False
+min_found = 9999999999
+minute = 0
+while minute < 1000 and status != 3:
+    minute += 1
+    next_map = move(blizzard,minute)
+    curr_set = set()
+    for p in p_set: # all previous steps
+        #print_map(next_map,p)
+        cc = can_walk(next_map,p)
+        if status == 0: # going to goal
+            if end_pos in cc:
+                print('reaching goal', minute, 'Part A')
+                part_a_time = time.time() - start
+                status = 1
+                turn_around = True
+
+        elif status == 1: # going to start
+            if end_pos in cc:
+                print('reaching start', minute)
+                status = 2
+                turn_around = True
+
+        elif status == 2: # going to goal
+            if end_pos in cc:
+                print('reaching goal again', minute, 'Part B')
+                part_b_time = time.time() - start
+                status = 3
+                turn_around = True
+
+        curr_set.update(cc)
+
+    if turn_around:
+        turn_around = False
+        p_set.clear()
+        p_set.add(end_pos)
+        if status == 1:
+            end_pos = (1,0)
+        elif status == 2:
+            end_pos = (w-2,h-1)
+    else:
+        p_set = curr_set
+
+print(f'Part A time: {part_a_time:.3f}')
+print(f'Part B time: {part_b_time:.3f}')
+
+'''
+Time Record O
+Part A time: 0.712
+Part B time: 2.505
+
+Part A time: 0.767
+Part B time: 2.436
+
+Part A time: 0.676
+Part B time: 2.285
+
+Part A time: 0.669
+Part B time: 2.372
+'''
